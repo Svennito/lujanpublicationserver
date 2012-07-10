61fe5584e1f2e9c445c5630c320610a789743ad7
svennito@gmail.com <svennito@gmail.com@7f9bd1de-d050-797e-e7f9-10d643674090>
2012-07-10 15:12:31 +0000

    Added shell scripts for queries and update of the database.

---
+++ trunk/bin/get_refs_instr.sh
@@ -0,0 +1,3 @@
+./gscholar.py -d -a  -e $1 -f $2 -t $2 -i $1.bib "$1 neutron"
+echo "Sleeping a minute to stay friends with google..."
+sleep 30
---
+++ trunk/bin/update_bibtex_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+update_bibtex.sh NPDF
+update_bibtex.sh SMARTS
+update_bibtex.sh HIPD
+update_bibtex.sh HIPPO
+update_bibtex.sh SCD
+update_bibtex.sh FDS
+update_bibtex.sh SPEAR
+update_bibtex.sh LQD
+update_bibtex.sh ASTERIX
+update_bibtex.sh PCS
+
+
