6ac5e5763cbf8b7fc3ea403dd5925c4bada03f5e
svennito@gmail.com <svennito@gmail.com@7f9bd1de-d050-797e-e7f9-10d643674090>
2012-07-23 17:33:08 +0000

    Added sample changer paper to HIPPO.bib, made comments for get_refs_author.sh

--- trunk/bin/get_refs_author.sh
+++ trunk/bin/get_refs_author.sh
@@ -1,3 +1,13 @@
+# call with 
+# - author search string without spaces
+# - year
+# - instrument
+# - facility
+#
+# e.g.
+#
+# ./get_refs_author.sh "S*Vogel" $year HIPPO LUJAN
+#
 ./gscholar.py -d -a -f $2 -t $2 -i $3.bib -c $4 -n $3 -w "$1" "$1"
 echo "Sleeping a minute to stay friends with google..."
 sleep 30
