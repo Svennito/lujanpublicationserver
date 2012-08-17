e8cca690983fd497b60ea7558664b26e93711192
svennito <svennito@gmail.com>
2012-08-17 23:07:59 +0000

    Added backup script.

---
+++ bin/backup_bibtex.sh
@@ -0,0 +1,8 @@
+now=$(date +"%Y%m%d")
+filename=backup_bibtex_$now.sql
+echo "Filename : $filename"
+mysqldump -h localhost -u bibtex -pbibtex bibtex > $filename
+ls -lrth $filename
+echo "Done. Use"
+echo "mysql -u bibtex -pbibtex < $filename"
+echo "to restore."
