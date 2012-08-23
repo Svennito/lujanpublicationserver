29a532f2d4c6603f85bc162c61cd2e42a307201b
svennito@gmail.com <svennito@gmail.com@7f9bd1de-d050-797e-e7f9-10d643674090>
2012-08-23 15:19:14 +0000

    Added facility to beam line search. Added volme, number and pages to bibtex key to make it more unambigous. Removed instrument from author search.

--- trunk/bin/get_refs_author.sh
+++ trunk/bin/get_refs_author.sh
@@ -8,6 +8,8 @@
 #
 # ./get_refs_author.sh "S*Vogel" $year HIPPO LUJAN
 #
-./gscholar.py -d -a -f $2 -t $2 -i $3.bib -c $4 -n $3 -w "$1" "$1"
+#./gscholar.py -d -a -f $2 -t $2 -i $3.bib -c $4 -n $3 -w "$1" "$1"
+# don't make instrument entry for named search as it might not be related to the instrument!
+./gscholar.py -d -a -f $2 -t $2 -i $3.bib -c $4 -w "$1" "$1"
 echo "Sleeping a minute to stay friends with google..."
 sleep 30
