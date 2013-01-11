7075f192d5d1371c9baae1ed30fbf33d2f19c4a9
svennito@gmail.com <svennito@gmail.com@7f9bd1de-d050-797e-e7f9-10d643674090>
2013-01-11 21:01:06 +0000

    update

--- trunk/bin/get_refs_instr.sh
+++ trunk/bin/get_refs_instr.sh
@@ -1,5 +1,5 @@
 #./gscholar.py -d -a  -e $1 -f $2 -t $2 -i $1.bib "$1 neutron"
 # make an instrument entry
-./gscholar.py -d -a  -e $1 -f $2 -t $2 -i $1.bib -n $1 -c $3 "$1 neutron"
+./gscholar.py -d -a  -e $1 -f $2 -t $2 -i $1.bib -n $1 -c $3 "\"$1\" neutron"
 echo "Sleeping a minute to stay friends with google..."
 sleep 30
