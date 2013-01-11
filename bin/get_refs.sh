62fef4b6102a0f6245c6de6686b69448e2e89777
svennito <svennito@gmail.com>
2013-01-11 21:01:06 +0000

    update

--- bin/get_refs.sh
+++ bin/get_refs.sh
@@ -1,4 +1,5 @@
-for instr in NPDF SMARTS HIPD HIPPO SCD FDS SPEAR LQD ASTERIX PCS PHAROS; do
+#for instr in NPDF SMARTS HIPD HIPPO SCD FDS SPEAR LQD ASTERIX PCS PHAROS; do
+for instr in SPEAR LQD ASTERIX PCS PHAROS; do
   for year in {2000..2012}; do
     ./get_refs_instr.sh $instr $year  LUJAN
   done
