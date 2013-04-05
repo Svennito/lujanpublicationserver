2b8fa6dce16c47086ea56d27e8d1aeb77bc36e49
svennito@gmail.com <svennito@gmail.com@7f9bd1de-d050-797e-e7f9-10d643674090>
2013-04-05 23:05:19 +0000

    changed database scheme to include new fields.

--- trunk/bin/get_refs.sh
+++ trunk/bin/get_refs.sh
@@ -1,6 +1,6 @@
 #for instr in NPDF SMARTS HIPD HIPPO SCD FDS SPEAR LQD ASTERIX PCS PHAROS; do
 for instr in SMARTS HIPD HIPPO SCD FDS SPEAR LQD ASTERIX PCS PHAROS; do
-  for year in {2010..2013}; do
+  for year in {2009..2013}; do
     ./get_refs_instr.sh $instr $year  LUJAN
   done
 done
