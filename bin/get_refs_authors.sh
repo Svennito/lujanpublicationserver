318caeac681882f38e607186166993168217a293
svennito <svennito@gmail.com>
2013-04-11 13:18:31 +0000

    updated search, finished harvest.

--- bin/get_refs_authors.sh
+++ bin/get_refs_authors.sh
@@ -1,4 +1,4 @@
-for year in {2013..2013}; do
+for year in {2012..2013}; do
   ./get_refs_author.sh "S*Vogel" $year HIPPO LUJAN
   ./get_refs_author.sh "J*Zhang" $year HIPPO LUJAN
   ./get_refs_author.sh "H*Xu" $year HIPPO LUJAN
@@ -55,4 +55,17 @@ done
 for year in {2000..2013}; do
   ./get_refs_author.sh "J*Rhyne" $year MANAGEMENT LUJAN
   ./get_refs_author.sh "P*Lewis" $year MANAGEMENT LUJAN
+done
+
+
+for year in {2010..2013}; do
+  ./get_refs_author.sh "L*Balogh" $year SMARTS LUJAN
+done
+
+for year in {2008..2013}; do
+  ./get_refs_author.sh "J*Siewenie" $year NPDF LUJAN
+done
+
+for year in {2011..2013}; do
+  ./get_refs_author.sh "C*White" $year NPDF LUJAN
 done
