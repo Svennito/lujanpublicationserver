2c283204152aaf6812a87da8f1db97ecfa4694ee
svennito@gmail.com <svennito@gmail.com@7f9bd1de-d050-797e-e7f9-10d643674090>
2013-04-17 02:44:09 +0000

    RUn search again for gscholar members. Added scripts to extract certain journals.

---
+++ trunk/bin/export_bibtex_journals.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ "$1" == "" ]; then
+  echo "Please call with journal (in double quotes), start year, end year."
+  exit
+fi
+
+if [ "$2" == "" ]; then
+  echo "Please call with journal (in double quotes), start year, end year."
+  exit
+fi
+
+if [ "$3" == "" ]; then
+  echo "Please call with journal (in double quotes), start year, end year."
+  exit
+fi
+
+echo "Exporting papers in <$1> published between $2 and $3."
+echo "select entry from bibtab where journal like '$1' AND year between $2 and $3 AND isnull(falsehit);" > temp.sql
+
+filename=$1
+# issue the query to mysql
+mysql --skip-column-names -ubibtex -pbibtex bibtex < temp.sql > temp.bib
+
+# clean up the bib file
+sed  "s/\\\n/\\`echo -e '\r'`/g" temp.bib > export_${filename// /_}_$2_$3.bib
+
+# make a nice html file out of this
+java -jar ~/Downloads/JabRef-2.9.2.jar -n -i export_${filename// /_}_$2_$3.bib -o export_${filename// /_}_$2_$3.html,lujan
---
+++ trunk/bin/export_bibtex_journals_all.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+./export_bibtex_journals.sh "Science" 2009 2013
+./export_bibtex_journals.sh "Nature" 2009 2013
+./export_bibtex_journals.sh "Physical Review Letters" 2009 2013
+./export_bibtex_journals.sh "Cell" 2009 2013
+./export_bibtex_journals.sh "Journal of the American Chemical Society" 2009 2013
+./export_bibtex_journals.sh "Proceedings of the National Academy of Sciences USA" 2009 2013
+./export_bibtex_journals.sh "Nano Letters" 2009 2013
+./export_bibtex_journals.sh "Nature Chemical Biology" 2009 2013
+./export_bibtex_journals.sh "Nature Chemistry" 2009 2013
+./export_bibtex_journals.sh "Nature Geoscience" 2009 2013
+./export_bibtex_journals.sh "Nature Materials" 2009 2013
+./export_bibtex_journals.sh "Nature Nanotechnology" 2009 2013
+./export_bibtex_journals.sh "Nature Photonics" 2009 2013
+./export_bibtex_journals.sh "Nature Physics" 2009 2013
+./export_bibtex_journals.sh "Nature Structural and Molecular Biology" 2009 2013
+./export_bibtex_journals.sh "Angewandte Chemie International Edition" 2009 2013
+./export_bibtex_journals.sh "Advanced Materials" 2009 2013
+./export_bibtex_journals.sh "Environmental Science and Technology" 2009 2013
+./export_bibtex_journals.sh "Applied Physics Letters" 2009 2013
+./export_bibtex_journals.sh "EMBO Journal" 2009 2013
